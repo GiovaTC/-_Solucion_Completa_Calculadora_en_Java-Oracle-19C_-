82067807e46bca3a0e409607e3e616844c873096
gtapiero <giovanny.tapiero@pi.edu.co>
2026-04-06 10:42:56 -0500

    commit - Main .

--- src/CalculadoraControlador.java
+++ src/CalculadoraControlador.java
@@ -21,7 +21,38 @@ public class CalculadoraControlador {
         vista.btnDividir.addActionListener(e -> calcular("/"));
     }
 
-    private void calcular(String s) {
-    }
+    private void calcular(String op) {
+        try {
+            double n1 = Double.parseDouble(vista.txtNum1.getText());
+            double n2 = Double.parseDouble(vista.txtNum2.getText());
+            double resultado = 0;
 
+            switch (op) {
+                case "+":
+                    resultado = n1 + n2;
+                    break;
+                case "-":
+                    resultado = n1 - n2;
+                    break;
+                case "*":
+                    resultado = n1 * n2;
+                    break;
+                case "/":
+                    if (n2 == 0) {
+                        vista.lblResultado.setText("error: division por 0");
+                        return;
+                    }
+                    resultado = n1 / n2;
+                    break;
+            }
+
+            vista.lblResultado.setText("Resultado: " + resultado);
+
+            // guardar en BD
+            dao.guardar(n1, n2, op, resultado);
+
+        } catch (Exception e) {
+            vista.lblResultado.setText("error: datos invalidos" + e.getMessage());
+        }
+    }
 }
